15184f3e92bcae71b1f8a7fbcac8a79a1a608ab4
Joshua Prpich <joshuaprpich@gmail.com>
2018-01-10 20:47:40 -0500

    fix for production

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,18 +1,28 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
+     user = User.find(current_user.id)
+     
      @expenses = Expense.order("date DESC")
       
       if params[:concept].present?
         @expenses = @expenses.where("concept LIKE :query", query: "%#{params[:concept]}%")
+        @expenses = @expenses.where(user_id: current_user.id).order("date DESC")
       end
 
 
       
       if (params[:category_id].present?)
         @expenses = @expenses.where("category_id LIKE :query", query: "%#{params[:category_id]}%")
+        @expenses = @expenses.where(user_id: current_user.id).order("date DESC")
+
       end
 
+         
+
   end
+
+
 end
 
 
